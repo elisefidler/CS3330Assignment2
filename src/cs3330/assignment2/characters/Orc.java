43d7176074b903550b30a8087ef6e24249b70429
elisefidler <efidler255@gmail.com>
2025-03-05 23:35:47 -0600

    Created getRace() method for Orc class that returns character's race. Overrides super method.

--- src/cs3330/assignment2/characters/Orc.java
+++ src/cs3330/assignment2/characters/Orc.java
@@ -1,5 +1,18 @@
 package cs3330.assignment2.characters;
 
 public class Orc extends MiddleEarthCharacter {
+	
+	/**
+	 * Parameterized constructor for Orc class.
+	 * 
+	 *  Initializes variables using user input.
+	 *  
+	 *  @param name - The character's name
+	 *  @param health - The character's health points
+	 *  @param power - The character's power level/damage they can do
+	 */
+	public Orc(String name, double health, double power) {
+		super(name, health, power);
+	}
 
 }
