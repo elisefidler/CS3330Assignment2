9cb9617be5598188e0f5bebe8f24666725095e14
elisefidler <efidler255@gmail.com>
2025-03-05 23:46:37 -0600

    Created Singleton that returns instance of MiddleEarthCouncil.

--- src/cs3330/assignment2/council/MiddleEarthCouncil.java
+++ src/cs3330/assignment2/council/MiddleEarthCouncil.java
@@ -1,9 +1,26 @@
 package cs3330.assignment2.council;
 import cs3330.assignment2.characters.*;
+import cs3330.project2.council.MiddleEarthCouncil;
 
 /**
  * Singleton that returns instance of MiddleEarthCouncil and provides access to class CharacterManager.
  */
 public class MiddleEarthCouncil {
+	
+	private static MiddleEarthCouncil instance;
+	
+	private MiddleEarthCouncil() {}
+	
+/**
+ * 	Returns single instance of MiddleEarthCouncil.
+ */
+	public static MiddleEarthCouncil getInstance() {
+		if (instance == null) {
+			
+			instance = new MiddleEarthCouncil();
+		}
+		
+		return instance;
+	}
 
 }
