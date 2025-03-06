0ee56228f98de982663efa2eac84f95f6cc638aa
elisefidler <efidler255@gmail.com>
2025-03-05 23:31:36 -0600

    Created parameterized construtor for Human class that initializes name, health, and power.

--- src/cs3330/assignment2/characters/Human.java
+++ src/cs3330/assignment2/characters/Human.java
@@ -1,5 +1,19 @@
 package cs3330.assignment2.characters;
 
 public class Human extends MiddleEarthCharacter {
+	
+	/**
+	 * Parameterized constructor for Human class.
+	 * 
+	 *  Initializes variables using user input.
+	 *  
+	 *  @param name - The character's name
+	 *  @param health - The character's health points
+	 *  @param power - The character's power level/damage they can do
+	 */
+	public Human(String name, double health, double power) {
+		super(name, health, power);
+		// TODO Auto-generated constructor stub
+	}
 
 }
