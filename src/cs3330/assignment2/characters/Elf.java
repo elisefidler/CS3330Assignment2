e2536ce2d20f6e19323442436d15cf1ee8d3ab3d
elisefidler <efidler255@gmail.com>
2025-03-05 23:25:48 -0600

    Created parameterized constructor for Elf that initializes name, health, and power.

--- src/cs3330/assignment2/characters/Elf.java
+++ src/cs3330/assignment2/characters/Elf.java
@@ -1,5 +1,19 @@
 package cs3330.assignment2.characters;
 
 public class Elf extends MiddleEarthCharacter {
+	
+	/**
+	 * Parameterized constructor for Elf class.
+	 * 
+	 *  Initializes variables using user input.
+	 *  
+	 *  @param name - The character's name
+	 *  @param health - The character's health points
+	 *  @param power - The character's power level/damage they can do
+	 */
+	public Elf(String name, double health, double power) {
+		super(name, health, power);
+		// TODO Auto-generated constructor stub
+	}
 
 }
