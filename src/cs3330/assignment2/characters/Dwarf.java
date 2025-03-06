da927fce1c10fa31f3bc87eaafdb6fd4fd8c2192
elisefidler <efidler255@gmail.com>
2025-03-05 23:12:52 -0600

    Created paramatrized constructor for Dwarf class that initializes name, hp, and power.

--- src/cs3330/assignment2/characters/Dwarf.java
+++ src/cs3330/assignment2/characters/Dwarf.java
@@ -1,5 +1,19 @@
 package cs3330.assignment2.characters;
 
-public class Dwarf extends MiddleEarthCharacter{
+public class Dwarf extends MiddleEarthCharacter {
+	
+	/**
+	 * Parameterized constructor for Dwarf class.
+	 * 
+	 *  Initializes variables using user input.
+	 *  
+	 *  @param name - The character's name
+	 *  @param health - The character's health points
+	 *  @param power - The character's power level/damage they can do
+	 */
+	public Dwarf(String name, double health, double power) {
+		super(name, health, power);
+		// TODO Auto-generated constructor stub
+	}
 
 }
