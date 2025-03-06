0c953137dd5b78cdbee30a1bd6887b4a78680567
elisefidler <efidler255@gmail.com>
2025-03-05 23:05:01 -0600

    Created constructor, getters, and setters for MiddleEarthCharacter's name, power, and health.

--- src/cs3330/assignment2/characters/MiddleEarthCharacter.java
+++ src/cs3330/assignment2/characters/MiddleEarthCharacter.java
@@ -1,5 +1,70 @@
 package cs3330.assignment2.characters;
 
 public abstract class MiddleEarthCharacter {
+	private String name;
+	private double health;
+	private double power;
+	
+	/**
+	 * Parameterized constructor for MiddleEarthCharacter class.
+	 * 
+	 *  Initializes variables using user input.
+	 *  
+	 *  @param name - The character's name
+	 *  @param health - The character's health points
+	 *  @param power - The character's power level/damage they can do
+	 */	
+	public MiddleEarthCharacter(String name, double health, double power) {
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+	
+	/**
+	 * Gets character's name.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets character's name.
+	 * 
+	 * @param name - The character's name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Gets character's health.
+	 */
+	public double getHealth() {
+		return health;
+	}
+	
+	/**
+	 * Sets character's health.
+	 * 
+	 * @param health - The character's health points
+	 */
+	public void setHealth(double health) {
+		this.health = health;
+	}
 
+	/**
+	 * Gets character's power.
+	 */
+	public double getPower() {
+		return power;
+	}
+	
+	/**
+	 * Sets character's power.
+	 * 
+	 * @param power - The character's power level/damage they can do
+	 */
+	public void setPower(double power) {
+		this.power = power;
+	}
 }
