ad21e51e02be78b3c86f22c3eaec587595747804
elisefidler <efidler255@gmail.com>
2025-03-05 23:44:26 -0600

    Created package and class MiddleEarthCouncil.

---
+++ src/cs3330/assignment2/council/MiddleEarthCouncil.java
@@ -0,0 +1,9 @@
+package cs3330.assignment2.council;
+import cs3330.assignment2.characters.*;
+
+/**
+ * Singleton that returns instance of MiddleEarthCouncil and provides access to class CharacterManager.
+ */
+public class MiddleEarthCouncil {
+
+}
