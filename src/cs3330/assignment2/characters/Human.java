cc09fc03b95630f2ebd497146bbb77b0d0fdabdb
elisefidler <efidler255@gmail.com>
2025-03-05 23:33:54 -0600

    Created attack method for Human class that simulates attack on target character based on characters' races. Overrrides super method.

--- src/cs3330/assignment2/characters/Human.java
+++ src/cs3330/assignment2/characters/Human.java
@@ -24,5 +24,42 @@ public class Human extends MiddleEarthCharacter {
 	public String getRace() {
 		return(this.getClass().getSimpleName());
 	}
+	
+	/**
+	 * Overrides attack(MiddleEarthCharacter) method. Displays unique attack rule based on
+	 * character's race and the race of the character they are attacking. Returns true if 
+	 * attacks reduces target’s health. Returns false if attack does 0 damage.
+	 * 
+	 * @param target - The character being attacked.
+	 */
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		double hp = target.getHealth();
+		double damage = this.getPower();
+		String r = target.getRace();
+		
+		if (r.equals("Orc") || r.equals("Human")) {
+			
+			System.out.println(this.getName() + " whiffed a punch at " +  target.getName() + " for 0 damage!");
+			
+			return false;
+		}
+		if (r.equals("Wizard")) {
+			
+			target.setHealth(hp - (damage*1.5));
+			
+			System.out.println(this.getName() + "'s attack was super effective! They karate chopped " + target.getName() + " for " + damage*1.5 + " damage.");
+			
+			return true;
+		}
+		else {
+			
+			target.setHealth(hp - damage);
+			
+			System.out.println(this.getName() + "'s attack was effective! They sucker-punched " + target.getName() + " for " + damage + " damage.");
+			
+			return true;
+		}
+	}
 
 }
