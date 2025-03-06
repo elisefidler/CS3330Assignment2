cf493e2e0cac8ce20d51a74d0c1ee53c3e585614
elisefidler <efidler255@gmail.com>
2025-03-05 23:29:03 -0600

    Created method attack for Elf that simulates attack on target character based on characters' races. Overrides super method.

--- src/cs3330/assignment2/characters/Elf.java
+++ src/cs3330/assignment2/characters/Elf.java
@@ -24,4 +24,41 @@ public class Elf extends MiddleEarthCharacter {
 	public String getRace() {
 		return(this.getClass().getSimpleName());
 	}
+	
+	/**
+	 * Overrides attack(MiddleEarthCharacter) method. Displays unique attack rule based on
+	 * character's race and the race of the character they are attacking. Returns true if 
+	 * attacks reduces target’s health. Returns false if attack does 0 damage.
+	 * 
+	 * @param target - The character being attacked.
+	 */
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		double hp = target.getHealth();
+		double damage = this.getPower();
+		String r = target.getRace();
+		
+		if (r.equals("Elf") || r.equals("Dwarf")) {
+			
+			System.out.println(this.getName() + " accidently misspoke a spell at " +  target.getName() + " for 0 damage!");
+			
+			return false;
+		}
+		if (r.equals("Orc")) {
+		     
+		    target.setHealth(hp - (damage*1.5));
+		    
+			System.out.println(this.getName() + "'s attack was super effective! They summoned a hoard of rabid fairies to attack " + target.getName() + " for " + damage*1.5 + " damage.");
+		    
+			return true;
+		}
+		else {
+			
+			target.setHealth(hp - damage);
+			
+			System.out.println(this.getName() + "'s attack was effective! They conjured a wild beast to attack " + target.getName() + " for " + damage + " damage.");
+			
+			return true;
+		}
+	}
 }
