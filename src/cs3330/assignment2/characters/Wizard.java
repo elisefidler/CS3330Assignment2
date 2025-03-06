f21438562c7edca546d693f4847fabc97d73a7ef
elisefidler <efidler255@gmail.com>
2025-03-05 23:40:14 -0600

    Created parameterized contructor for Wizard class that initializes name, health, and power.

--- src/cs3330/assignment2/characters/Wizard.java
+++ src/cs3330/assignment2/characters/Wizard.java
@@ -2,5 +2,18 @@ package cs3330.assignment2.characters;
 
 public class Wizard extends MiddleEarthCharacter {
 	
+	/**
+	 * Parameterized constructor for Wizard class.
+	 * 
+	 *  Initializes variables using user input.
+	 *  
+	 *  @param name - The character's name
+	 *  @param health - The character's health points
+	 *  @param power - The character's power level/damage they can do
+	 */
+	public Wizard(String name, double health, double power) {
+		super(name, health, power);
+		// TODO Auto-generated constructor stub
+	}
 
 }
